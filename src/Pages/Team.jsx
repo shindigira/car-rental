63e123a6cd969960c400dd8d437daa832b1e783d
shindigira <shindigira@gmail.com>
2023-03-20 10:52:01 -0400

    spelling

--- src/Pages/Team.jsx
+++ src/Pages/Team.jsx
@@ -9,12 +9,12 @@ import Person6 from "../images/team/6.png";
 
 function Team() {
   const teamPpl = [
-    { img: Person1, name: "Luke Miller", job: "Salesman" },
+    { img: Person1, name: "Luke Miller", job: "Sales Representative" },
     { img: Person2, name: "Michael Diaz", job: "Business Owner" },
     { img: Person3, name: "Briana Ross", job: "Photographer" },
     { img: Person4, name: "Lauren Rivera", job: "Car Detailist" },
     { img: Person5, name: "Martin Rizz", job: "Mechanic" },
-    { img: Person6, name: "Caitlyn Hunt", job: "Menager" },
+    { img: Person6, name: "Caitlyn Hunt", job: "Manager" },
   ];
   return (
     <>
